caa23b19789e41be04c8283462e489e570505952
Tirthankar Halder <tirthankarhalder70@gmail.com>
2024-12-25 11:59:39 +0530

    iperf main added

--- iperf/main.py
+++ iperf/main.py
@@ -1,3 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 20:56:18 2023
+
+@author: xd
+"""
+from pathlib import Path
+import pathlib
+import shlex
+import subprocess
+from datetime import datetime
+import os
+import sys
+
+cmds_file = pathlib.Path(__file__).with_name("cmdserver.txt")
+file_name = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")+".txt"
+Path(file_name).touch(exist_ok= True)
+output_file = pathlib.Path(__file__).with_name(file_name)
+with open(cmds_file, encoding="utf-8") as commands, open(output_file, "w", encoding="utf-8") as output:
+    for command in commands:
+        print(command)
+        command = shlex.split(command.strip())
+        output.write(f"{datetime.now()}\n# {shlex.join(command)}\n")
+        output.flush()
+        #subprocess.run(command, stdout=output, stderr=subprocess.STDOUT, encoding="utf-8")
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+        for line in process.stdout:
+                sys.stdout.write(line)
+                output.write(line)
+        for line in process.stderr:
+                sys.stderr.write(line)
+                output.write(line)
+        process.wait()
+'''
+ans = input("Do you want to keep the file? yes/no: ")
+if ans == "no":
+        os.system(f"rm {file_name}")
+        print("File deleted sucessfully")
+'''
+        
+
+"""
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 12 20:56:18 2023
@@ -18,3 +61,5 @@ with open(cmds_file, encoding="utf-8") as commands, open(output_file, "w", encod
         output.write(f"{datetime.now()}\n# {shlex.join(command)}\n")
         output.flush()
         subprocess.run(command, stdout=output, stderr=subprocess.STDOUT, encoding="utf-8",shell=True)
+        
+"""
